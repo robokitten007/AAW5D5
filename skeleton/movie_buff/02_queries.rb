be0e1c4a345bf1a17728752ce40c7370082d7443
wliu <wliu0709@gmail.com>
2021-05-21 17:28:00 -0400

    start 02 queries

--- skeleton/movie_buff/02_queries.rb
+++ skeleton/movie_buff/02_queries.rb
@@ -2,13 +2,22 @@ def eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between
   # 3 and 5 (inclusive).
   # Show the id, title, year, and score.
+  Movie
+  .select(:id, :title, :yr, :score)
+  .where(yr: 1980..1989)
+  .where(score: 3..5)
 
 end
 
 def bad_years
   # List the years in which a movie with a rating above 8 was not released.
-
+Movie
+.select(:year, Maxium(score)  )
+.group(:year)
+.having(maxium(:score) < 8 )
 end
+
+  
 
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
